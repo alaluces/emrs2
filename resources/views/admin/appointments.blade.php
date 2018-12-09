63d62bc4675fdab6dbd89e6ed62de1d9f714556d
alaluces <aries_laluces@yahoo.com>
2018-12-09 06:52:04 +0100

    start treatment modal

--- resources/views/admin/appointments.blade.php
+++ resources/views/admin/appointments.blade.php
@@ -9,7 +9,8 @@
         </h1>
     </div>
 @stop
-{{-- dd($patients) --}}
+{{ dd($appointments) }}
+
 @section('content')
   <div class="page-content browse container-fluid">
       @include('voyager::alerts')
@@ -34,7 +35,7 @@
                             <td>{{ $appointment->first_name }} {{ $appointment->last_name }}</td>
                             <td>{{ $appointment->appt_type }}</td>
                             <td>
-                              <a href="javascript:;" class="btn btn-sm btn-success">
+                              <a href="javascript:;" class="btn btn-sm btn-success start" data-id="{{ $appointment->id }}">
                               <i class="voyager-activity"></i> Start treatment</a>
 
                               <a href="javascript:;" class="btn btn-sm btn-danger cancel" data-id="{{ $appointment->id }}">
@@ -97,6 +98,25 @@
     </div>
 
 
+    {{-- start treatment modal --}}
+    <div class="modal modal-success fade" tabindex="-1" id="start_modal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager::generic.close') }}"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><i class="voyager-calendar"></i> Start patient treatment?</h4>
+                </div>
+                <div class="modal-footer">
+                    <form action="#" id="start_form" method="POST">
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-success pull-right " value="Yes, start treatment">
+                    </form>
+                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Cancel</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
     {{-- cancel appointment modal --}}
     <div class="modal modal-danger fade" tabindex="-1" id="cancel_modal" role="dialog">
         <div class="modal-dialog">
@@ -121,6 +141,12 @@
 @section('javascript')
     <!-- DataTables -->
     <script>
+        $('td').on('click', '.start', function (e) {
+            $('#start_form')[0].action = '{{ URL::to('/admin/appointments/cancel/__id') }}'.replace('__id', $(this).data('id'));
+            //console.log($('#delete_form')[0].action);
+            $('#start_modal').modal('show');
+        });
+
         $('td').on('click', '.cancel', function (e) {
             $('#cancel_form')[0].action = '{{ URL::to('/admin/appointments/cancel/__id') }}'.replace('__id', $(this).data('id'));
             //console.log($('#delete_form')[0].action);
