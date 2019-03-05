5b6cc7b70b38e8d91b6ec2c7e98a59fcf4f6bc17
alaluces <aries_laluces@yahoo.com>
2019-03-05 08:44:34 +0000

    redirect to login if not authenticated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/emrs';
 
     /**
      * Create a new controller instance.
